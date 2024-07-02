123abd4f463c323287eddac71cb100a141926b77
StephenChips <tcw6991@gmail.com>
2024-07-02 19:29:37 +0800

    [wip] Created the /sign-up page's component (empty) and its route.

--- src/router.tsx
+++ src/router.tsx
@@ -4,6 +4,7 @@ import { MessageWindow } from "./pages/App/components/MessageWindow/MessageWindo
 import { NotificationWindow } from "./pages/NotificationWindow/NotificationWindow.tsx";
 import { Account } from "./pages/AccountSettings/AccountSettings.tsx";
 import { Login } from "./pages/Login/Login.tsx";
+import { SignUp } from "./pages/SignUp/SignUp.tsx"
 
 export const router = createBrowserRouter([
   {
@@ -27,5 +28,9 @@ export const router = createBrowserRouter([
   {
     path: "/login",
     element: <Login />
+  },
+  {
+    path: "/sign-up",
+    element: <SignUp />
   }
 ]);
