1d378978fa3a767ea203ffab483347f6195ac40c
StephenChips <tcw6991@gmail.com>
2024-07-09 19:00:50 +0800

    Set up Koa and Socket.IO.

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,33 @@
-import { runSQLFile } from "./database"
 import { resolve } from "path";
+import * as http from "http"
+import * as Koa from "koa"
+import * as serve from "koa-static"
+import * as SocketIO from "socket.io";
+
+import { runSQLFile } from "./database"
+
+const PORT = 8080
 
 async function main() {
-  const result = await runSQLFile(resolve(__dirname, "../sql/init.sql"));
-  console.log(result)
+  console.log("Creating database (if it hasn't been created yet)")
+  await runSQLFile(resolve(__dirname, "../sql/init.sql"));
+  console.log("Database is created.");
+
+  console.log()
+
+  console.log("Starting the server")
+  const app = new Koa();
+  app.use(serve(resolve(__dirname, "../public")));
+  const httpServer = http.createServer(app.callback())
+  const io = new SocketIO.Server(httpServer);
+  io.on("connection", onSocketIOConnection)
+  httpServer.listen(PORT, () => {
+    console.log("The server is started at the port " + PORT);
+  })
+}
+
+function onSocketIOConnection(socket: SocketIO.Socket) {
+  socket.send("hello, world")
 }
 
 main()
