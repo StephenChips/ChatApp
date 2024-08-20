0ea5d759c118e96332cfb3abd7db0dc5cc42da4d
StephenChips <tcw6991@gmail.com>
2024-08-20 15:24:01 +0800

    Added vite-bundle-analyzer

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { analyzer } from "vite-bundle-analyzer"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), analyzer()],
   server: {
     proxy: {
       "/api": "http://127.0.0.1:8080",
