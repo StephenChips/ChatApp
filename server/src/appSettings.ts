2435a333313379d23a5b86ffaa8d6ca24ec6a7b6
StephenChips <tcw6991@gmail.com>
2024-09-13 21:28:08 +0800

    Added options `jwtSecretFile` and `postgreSQL.passwordFile` to the settings file.
    1. `jwtSecretFile`:
        Specify a path to a file that contains the app's jwt secret.
    2. `postgreSQL.passwordFile`:
        Specify a path to a file that contains app's database's password.

--- server/src/appSettings.ts
+++ server/src/appSettings.ts
@@ -1,4 +1,5 @@
 import Joi = require("joi");
+import { readFileSync } from "node:fs";
 
 interface AppSettings {
   readonly jwtSecret: string;
@@ -8,30 +9,32 @@ interface AppSettings {
     databse: string;
     host: string;
     port: number;
-  }
+  };
   readonly http: {
     port: number;
-  }
+  };
   readonly https: {
     port: number;
     certPath: string;
     keyPath: string;
-  }
+  };
 }
 
 const settingsJSONSchema = Joi.object({
   jwtSecret: Joi.string(),
+  jwtSecretFile: Joi.string(),
 
   postgreSQL: Joi.object({
-    user: Joi.string(),
+    user: Joi.string().required(),
     password: Joi.string(),
-    database: Joi.string(),
-    host: Joi.string().default("127.0.0.1"),
-    port: Joi.number().default(5432),
-  }).options({ presence: "required" }),
+    passwordFile: Joi.string(),
+    database: Joi.string().required(),
+    host: Joi.string().default("127.0.0.1").required(),
+    port: Joi.number().default(5432).required(),
+  }).xor("password", "passwordFile"),
 
   http: Joi.object({
-    port: Joi.number().default(80)
+    port: Joi.number().default(80),
   }),
 
   https: Joi.object({
@@ -41,12 +44,35 @@ const settingsJSONSchema = Joi.object({
   })
     .options({ presence: "required" })
     .optional(),
-});
+}).xor("jwtSecret", "jwtSecretFile");
 
 function initAppSettings(): AppSettings {
   const settingsJSON = require("../settings.json");
 
-  Joi.assert(settingsJSON, settingsJSONSchema, "The settings.json is invalid: ");
+  try {
+    Joi.assert(
+      settingsJSON,
+      settingsJSONSchema,
+      "The settings.json is invalid: "
+    );
+  } catch (e) {
+    const error = e as Error;
+    console.error(error.message);
+    process.exit(1);
+  }
+
+  if (settingsJSON.hasOwnProperty("jwtSecretFile")) {
+    settingsJSON.jwtSecret = readFileSync(settingsJSON.jwtSecretFile, "utf-8");
+    delete settingsJSON.jwtSecretFile;
+  }
+
+  if (settingsJSON.postgreSQL.hasOwnProperty("passwordFile")) {
+    settingsJSON.postgreSQL.password = readFileSync(
+      settingsJSON.postgreSQL.passwordFile,
+      "utf-8"
+    );
+    delete settingsJSON.postgreSQL.passwordFile;
+  }
 
   return settingsJSON;
 }
